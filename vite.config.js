3216f59eacf7085afc8963726604675d6e0b86c0
devloper51 <mastercanva16@gmail.com>
2025-05-11 14:26:22 +0530

    fix the config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'], // Add this line to mark react-router-dom as external
+    },
+  },
 })
